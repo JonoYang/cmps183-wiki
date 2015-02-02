fa6796756b0324f45f92885fd0ec71cc2a8dd8ab
Jonathan Yang <the.jonathan.yang@gmail.com>
2015-02-02 12:12:40 -0800

    started table entries

--- models/tables.py
+++ models/tables.py
@@ -8,6 +8,7 @@ RE_LINKS = re.compile('(<<)(.*?)(>>)')
 
 db.define_table('pagetable' # Name 'page' is reserved unfortunately.
     # Complete!
+    Field('title')
     )
 
 
@@ -20,6 +21,9 @@ db.define_table('testpage',
     # This table is used for testing only.  Don't use it in your code,
     # but feel free to look at how I use it. 
     Field('body', 'text'),
+    Field('author', 'text'),
+    Field('datetime'),
+    Field('content', 'text')
     )
 
 def create_wiki_links(s):
