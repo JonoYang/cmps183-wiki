c48680f319e2b67fe91c94a4cbb7bf726ec4d5a2
Jonathan Yang <the.jonathan.yang@gmail.com>
2015-02-03 20:26:45 -0800

    code cleanup

--- controllers/default.py
+++ controllers/default.py
@@ -6,11 +6,9 @@ import logging
 
 def index():
 
-    title = request.args(0) or 'main page'
+    title = request.args(0) or 'Main'
+    title = title.lower()
     display_title = title.title()
-
-    if title == 'main page':
-        title = 'main_page'
 
     if db(db.pagetable.title == title).select().first() is None:
         redirect(URL('default', 'new', args=[title]))
@@ -18,40 +16,25 @@ def index():
     page_id = db(db.pagetable.title == title).select().first().id
 
     rev = db(db.revision.pagetable_id == page_id).select(orderby=~db.revision.date_created).first()
-
-    s = rev.body if rev is not None else ''
+    s = rev.body
 
     editing = request.vars.edit == 'y'
 
     if editing:
-        # We are editing.  Gets the body s of the page.
-        # Creates a form to edit the content s, with s as default.
         form = SQLFORM.factory(Field('body', 'text',
                                      label='Content',
                                      default=s
                                      ))
-        # You can easily add extra buttons to forms.
         form.add_button('Cancel', URL('default', 'index', args=[title]))
-        # Processes the form.
+
         if form.process().accepted:
-            # Writes the new content.
-            if rev is None:
-                # First time: we need to insert it.
-                db.pagetable.insert(title=title)
-                db.revision.insert(author = db.auth_user, body=form.vars.body, pagetable_id = page_id)
-            else:
-                # We update it.
-                rev.update_record(body=form.vars.body)
-            # We redirect here, so we get this page with GET rather than POST,
-            # and we go out of edit mode.
+            rev.update_record(body=form.vars.body)
             redirect(URL('default', 'index', args=[title]))
+
         content = form
     else:
-        # We are just displaying the page
         content = s
 
-    # Here, I am faking it.  
-    # Produce the content from real database data. 
     button = A('edit', _class='btn', _href=URL('default', 'index', args=[title], vars=dict(edit='y')))
 
     return dict(display_title = display_title,
@@ -61,28 +44,28 @@ def index():
                 )
 
 def new():
-    new_page = request.args(0)
-    content = 'Would you like to create the page for %s?' % (new_page)
-    create_button = A('Create page', _class='btn', _href=URL('default', 'create', args=[new_page]))
+    title = request.args(0)
+    content = 'Would you like to create the page for %s?' % (title)
+    create_button = A('Create page', _class='btn', _href=URL('default', 'create', args=[title]))
     cancel_button = A('Cancel', _class='btn', _href=URL('default', 'index'))
     return dict(content = content,
                 create_button = create_button,
                 cancel_button = cancel_button,
-                new_page = new_page
+                title = title
                 )
 
 def create():
-    page_name = request.args(0)
+    title = request.args(0)
     form = SQLFORM.factory(Field('body', 'text',
                                   label='Content'
                                 ))
     form.add_button('Cancel', URL('default', 'index'))
     if form.process().accepted:
-        db.pagetable.insert(title=page_name)
-        page_id = db(db.pagetable.title == page_name).select().first().id 
+        db.pagetable.insert(title=title)
+        page_id = db(db.pagetable.title == title).select().first().id 
         db.revision.insert(author = auth.user_id, body=form.vars.body, pagetable_id = page_id)
-        redirect(URL('default', 'index', args=[page_name]))
-    return dict(form = form, page_name = page_name)
+        redirect(URL('default', 'index', args=[title]))
+    return dict(form = form, title = title)
 
 def test():
     """This controller is here for testing purposes only.
